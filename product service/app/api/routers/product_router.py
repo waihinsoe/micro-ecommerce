6887e9e9e1f9d58d17c8f0b151336a3a2f3f3e22
WyneZ <wynekyawhan@gmail.com>
2025-06-17 15:50:02 +0630

    done create prodcut feature

---
+++ product service/app/api/routers/product_router.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, HTTPException #type:ignore
+from schemas.product_schema import ProductCreate, ProductInDB, ProductUpdate
+from uuid import UUID
+from ..controllers import product_controller
+
+router = APIRouter()
+
+
+@router.get("/", response_model=list[ProductInDB])
+async def get_all_products():
+    return 
+
+@router.post("/", response_model=ProductCreate, status_code=201)
+async def create_product(data: ProductCreate):
+    return await product_controller.create_product(data)
+
+@router.get("/{product_id}", response_model=ProductInDB)
+async def get_product(product_id: UUID):
+    product = await product_controller.get_product(product_id)
+    if not product:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return product
+
+@router.put("/{product_id}", response_model=ProductInDB)
+async def put_product(product_id: UUID, data: ProductUpdate):
+    return 
+
+@router.patch("/{product_id}", response_model=ProductInDB)
+async def patch_product(product_id: UUID, data: ProductUpdate):
+    return
+
+@router.delete("/{product_id}", status_code=204)
+async def delete_product(product_id: UUID):
+    await product_controller.delete_product(product_id)
+    return
+
---
+++ product service/app/db/crud.py
@@ -0,0 +1,49 @@
+from .mongodb import get_collection, change_IdToStr
+from bson import ObjectId # type: ignore
+
+
+async def create(collection_name: str, data: dict):
+    collection = get_collection(collection_name)
+    result = await collection.insert_one(data)
+    return result
+
+async def update_one(collection_name: str, id: str, data: dict):
+    collection = get_collection(collection_name)
+    result = await collection.update_one({"_id": ObjectId(id)}, {"$set": data})
+    if result.modified_count == 1:
+        return True
+    return False
+
+async def get_one(collection_name: str, id: str):
+    collection = get_collection(collection_name)
+    doc = await collection.find_one({"_id": ObjectId(id)})
+    if doc:
+        print(doc, "found")
+        return doc
+    print(doc, "not found")
+    return doc
+
+async def get_all(collection_name: str):
+    collection = get_collection(collection_name)
+    cursor = collection.find({})
+    data_list: list = []
+    async for data in cursor:
+        data["_id"] = str(data["_id"])
+        data_list.append(data)
+    return data_list
+
+async def get_all_id(collection_name: str):
+    collection = get_collection(collection_name)
+    cursor = collection.find({})
+    id_list = []
+    async for doc in cursor:
+        id = change_IdToStr(doc)
+        id_list.append(id)
+    return id_list
+
+async def delete_one(collection_name: str, id: ObjectId):
+    collection = get_collection(collection_name)
+    result = await collection.delete_one({"_id": id})
+    if result.deleted_count == 1:
+        return True
+    return False
