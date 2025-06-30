1d4645d7f97bd025caddd2b7ce4315b934be2dd8
WyneZ <wynekyawhan@gmail.com>
2025-06-30 14:36:44 +0630

    add GET method and admin Delete method for image feature done

--- product-service/app/api/routers/image_router.py
+++ product-service/app/api/routers/image_router.py
@@ -15,31 +15,22 @@ async def upload_image(product_id: str, images: List[UploadFile]):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
-# @router.get("/{product_id}/images/", response_model=list[dict])
-# async def get_images(product_id: str):
-#     try:
-#         # Fetch images from Cloudinary
-#         resources = cloudinary.api.resources(type="upload", prefix=f"products/{product_id}/images", max_results=100)
-        
-#         # Return a list of image URLs and public IDs
-#         return [
-#             {
-#                 "image_url": resource['secure_url'],
-#                 "public_id": resource['public_id'],
-#                 "format": resource['format']
-#             } for resource in resources['resources']
-#         ]
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+@router.get("/{product_id}/images/", response_model=list[dict])
+async def get_images(product_id: str):
+    try:
+        return await image_controller.get_product_images(product_id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 # Fake Delete For Saler
-@router.put("/{product_id}/images/", status_code=204)
+@router.put("/{product_id}/images/", status_code=200)
 async def fake_delete_image(product_id: str):
     try:
         return await image_controller.fake_delete_product_image(product_id)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+# For admin user
 @router.delete("/{product_id}/images/", status_code=204)
 async def delete_image(product_id: str):
     try:
